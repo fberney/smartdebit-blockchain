82e52c968b2d728ab2ee49cef614b83c16a8ba8e
Michel Kaporin <michel.kaporin@gmail.com>
2016-09-07 15:18:18 +0200

    Basic CRUD operations implemented

--- contracts/smartdebit.sol
+++ contracts/smartdebit.sol
@@ -4,25 +4,46 @@ contract smartdebit {
         address beneficiary;
         uint fee;
         uint period;
+        bool exists;
     }
     
-    Account[] private accountsToPay;
+    mapping (address => Account) private accountsMapping;
+    // Mapping from account address to its index in the address array. Used to delete.
+    mapping (address => uint) private addressIndices;
+    address[] private accountAddresses; // All direct debit account addresses
     
-    function createDirectDebit(address account, uint fee, uint period)  {
-        // check if the direct debit is not set up for this account
-        var acc = new Account();
-        acc.beneficiary = account;
-        acc.fee = fee;
-        acc.period = period;
+    function createDirectDebit(address beneficiary, uint fee, uint period)  {
+        if (accountsMapping[beneficiary].exists == true) {
+            throw; // Account already has a direct debit setup against it
+        }
         
-        accountsToPay.add(acc);
-    }
-    
-    function pay() {
+        var acc = Account(beneficiary, fee, period, true);
+        accountsMapping[beneficiary] = acc;
         
+        accountAddresses.push(beneficiary);
+        addressIndices[beneficiary] = accountAddresses.length+1;
     }
     
-    function getGreeting() constant returns (string g) {
-        return greeting;
+    function getDirectDebit(address beneficiary) returns (uint fee, uint period) {
+        if (accountsMapping[beneficiary].exists != true) {
+            throw;
+        }
+        
+        fee = accountsMapping[beneficiary].fee;
+        period = accountsMapping[beneficiary].period;
+    }
+    
+    function getAllDirectDebits() returns (address[] addresses) {
+      addresses = accountAddresses;
+    }
+    
+    function deleteDirectDebit(address beneficiary) {
+        if (accountsMapping[beneficiary].exists != true) {
+            throw;
+        }
+        
+        delete accountsMapping[beneficiary];
+        var index = addressIndices[beneficiary]; // Getting index of the beneficiary in the address array
+        delete accountAddresses[index];
     }
 }
